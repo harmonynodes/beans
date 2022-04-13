0ae69401944206d5e3d374133788cc78c85bcb8b
harmonynodes <goluman149@gmail.com>
2022-04-13 03:20:47 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const config = {
+  chainId: 97,
+  contractAddress: "0x5C51aD71f962dbD7308fc342F581308CF307f2f1",
+  scanLink:
+    "https://testnet.bscscan.com/address/0x5C51aD71f962dbD7308fc342F581308CF307f2f1#code",
+};
